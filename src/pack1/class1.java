ebf248fff4374f8c1b9fc78d15b8ba2786726a24
monikakaul05 <75671277+monikakaul05@users.noreply.github.com>
2021-07-13 08:20:49 +0530

    M1

--- src/pack1/class1.java
+++ src/pack1/class1.java
@@ -3,7 +3,9 @@ package pack1;
 public class class1 {
 	public static void main(String[] args) {
 		System.out.println("1");
-		System.out.println("1.1");
+		System.out.println("1.1_added_new_chnages");
+		System.out.println("2_added_new_chnages");
+		
 	}
 	
 }
